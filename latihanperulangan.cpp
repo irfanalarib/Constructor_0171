684df5baf58f54a70ea809fbcc818260e94c2946
irfanalarib <irfanalarib7@gmail.com>
2025-05-17 16:13:54 +0700

    menambahkan constructor, method tampil

--- latihanperulangan.cpp
+++ latihanperulangan.cpp
@@ -7,4 +7,15 @@ private:
     string nama;
     int nim;
     float nilai;
-}
+
+public:
+    mahasiswa(string n, int i) : nama(n), nim(i), nilai(0) {}
+
+    void tampil() {
+        cout << "Nama Mahasiswa : " << nama << endl;
+        cout << "NIM            : " << nim << endl;
+        cout << "Nilai          : " << nilai << endl << endl;
+    }
+
+    friend class Dosen;
+};
