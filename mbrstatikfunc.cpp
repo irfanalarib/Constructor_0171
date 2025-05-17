0117fb7be722da8d5211bd1beffca6f6454eed1a
irfanalarib <irfanalarib7@gmail.com>
2025-05-17 15:31:58 +0700

    menambahkan inisialisasi objek mahasiswa dan panggil fungsi setNIM

--- mbrstatikfunc.cpp
+++ mbrstatikfunc.cpp
@@ -32,3 +32,11 @@ void mahasiswa::printAll(){
     cout << "Nama  = " <<nama<<endl;
     cout<<endl;
 }
+
+int main(){
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs2("Budi jatmiko");
+    mahasiswa::setNim(9); //mengakses nim melalui ststic member function "setNim"
+    mahasiswa mhs3("Andi Janu");
+    mahasiswa mhs4("Joko wahano");
+}
